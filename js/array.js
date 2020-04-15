1c83f20612d7e3a0e572fccf1e03101e5f4760f9
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-04-15 13:09:59 -0400

    Added more array methods

--- js/array.js
+++ js/array.js
@@ -265,5 +265,32 @@ const alphabeticalOrder = (str) =>
 const findVowels = (str, letters = ['a', 'e', 'i', 'o', 'u']) =>
   str.split('').filter((s) => letters.indexOf(s) > -1).length;
 
-console.log(findVowels('abcdef'));
-console.log(findVowels('abcdef', ['b', 'c', 'f']));
+// console.log(findVowels('abcdef'));
+// console.log(findVowels('abcdef', ['b', 'c', 'f']));
+
+// Convert an amount to coins.
+const countCoins = (money, coins = [25, 10, 5, 1]) => {
+  const totalCoins = [];
+  for (let i = 0; i < coins.length; i += 1) {
+    const thisCoinNum = Math.floor(money / coins[i]);
+    for (let y = 0; y < thisCoinNum; y += 1) {
+      totalCoins.push(coins[i]);
+    }
+    money -= coins[i] * thisCoinNum;
+  }
+  return totalCoins;
+};
+
+// console.log(countCoins(90));
+
+// Extract unique characters from a string
+const extractUniqueChars = (str) => {
+  return str
+    .split('')
+    .filter((item, index, arr) => arr.slice(index + 1).indexOf(item) === -1);
+};
+
+const extractUniqueCharsOtherWay = (str) => [...new Set(str.split(''))];
+
+console.log(extractUniqueChars('aaaabbbccc'));
+console.log(extractUniqueCharsOtherWay('zzzzyyyyxxxx'));
